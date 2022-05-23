e42388f1eacd5127e9147bcaca84106b1d0fd1a0
mikamada <mikamada17@gmail.com>
2022-05-23 14:17:47 +0700

    header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import userPic from '../../assets/pic_filled.png';
+import './header.scss';
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className="header">
+			<Link to={'/'}>
+				<div className="logo">Movie App</div>
+			</Link>
+			<div className="user-image">
+				<img src={userPic} alt="user" />
+			</div>
+		</div>
+	);
 };
 
 export default Header;
