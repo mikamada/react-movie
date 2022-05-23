94e262292116a7ca45ee2d94b796afcde0125ffb
mikamada <mikamada17@gmail.com>
2022-05-23 14:58:01 +0700

    feat: setup redux toolkit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import MovieDetail from './components/MovieDetail/MovieDetail';
 import PageNotFound from './components/PageNotFound/PageNotFound';
+import { store } from './features/store';
+import { Provider } from 'react-redux';
 
 function App() {
 	return (
 		<>
-			<Router>
-				<Header />
-				<div className="container">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/movie/:imdbID" element={<MovieDetail />} />
-						<Route path="*" element={<PageNotFound />} />
-					</Routes>
-				</div>
-				<Footer />
-			</Router>
+			<Provider store={store}>
+				<Router>
+					<Header />
+					<div className="container">
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/movie/:imdbID" element={<MovieDetail />} />
+							<Route path="*" element={<PageNotFound />} />
+						</Routes>
+					</div>
+					<Footer />
+				</Router>
+			</Provider>
 		</>
 	);
 }
---
+++ src/features/movies/movieSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+	movie: [],
+};
+
+const movieSlice = createSlice({
+	name: 'movies',
+	initialState,
+	reducers: {
+		addMovie: (state, { payload }) => {
+			state.movie = payload;
+		},
+	},
+});
+
+export const { addMovie } = movieSlice.actions;
+export const getAllMovies = (state) => state.movies.movies;
+export default movieSlice.reducer;
