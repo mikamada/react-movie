bf6513367196a29b083e5df6757b4983e57a1809
mikamada <mikamada17@gmail.com>
2022-05-23 14:18:21 +0700

    feat: add footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import './footer.scss';
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className="footer">
+			<div>Movie App</div>
+			<div>copyright 2022 by mikamada</div>
+		</div>
+	);
 };
 
 export default Footer;
